b87016cfd081e3feda99fbd28d8638ea3e5d98a3
Mu'aaz Joosuf <muaazjoosuf@gmail.com>
2022-06-01 03:03:40 +0200

    created base angular project with bootstrap enabled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { CarouselCardComponent } from './carousel-card/carousel-card.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
